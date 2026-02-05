ce80955abe7d1fadb3f4f0f6043a1ea795e18e32
cso66 <priegl.csongor@gmail.com>
2026-02-05 17:04:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ with tab6:
     
     # Oszlop: Animáció
     st.markdown("**Animáció:**")
-    components.iframe(f"{base_url}animacio_lassitva.html", widht=2500, height=1100)
+    components.iframe(f"{base_url}animacio_lassitva.html", width=2500, height=1100)
     
     # Sor3: Chord + Heat
     st.markdown("**Chord + Heatmap:**")
