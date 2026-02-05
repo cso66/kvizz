494ec2c92490b32058e78c8cfa3da14c65db3a1d
cso66 <priegl.csongor@gmail.com>
2026-02-05 15:50:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Kvíz Dashboard", layout="wide", height=800)
+st.set_page_config(page_title="Kvíz Dashboard", layout="wide")
 
 st.markdown("""
 # 🖥️ Kvíz vizualizációk
