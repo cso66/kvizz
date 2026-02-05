2bf0a4c667b511b6cb444d4f5ad1f1fdcd8c3dba
cso66 <priegl.csongor@gmail.com>
2026-02-05 17:06:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,19 +49,19 @@ with tab6:
     
     # Második: Tablázatos (column)
     st.markdown("**Tablázatos graf:**")
-    components.iframe(f"{base_url}tabla_graf.html", height=900)
+    components.iframe(f"{base_url}tabla_graf.html", height=1000)
     
     # Sor1: 2 Sankey (row)
     st.markdown("**Sankey diagramok:**")
     col1, col2 = st.columns(2)
-    with col1: components.iframe(f"{base_url}sankey2025_3.html", height=500)
-    with col2: components.iframe(f"{base_url}sankey2024_.html", height=500)
+    with col1: components.iframe(f"{base_url}sankey2025_3.html", height=1100)
+    with col2: components.iframe(f"{base_url}sankey2024_.html", height=1100)
     
     # Sor2: 2 Radar (row)
     st.markdown("**Radar diagramok:**")
     col1, col2 = st.columns(2)
-    with col1: components.iframe(f"{base_url}radar_2025.html", height=800)
-    with col2: components.iframe(f"{base_url}radar_2024.html", height=800)
+    with col1: components.iframe(f"{base_url}radar_2025.html", height=1100)
+    with col2: components.iframe(f"{base_url}radar_2024.html", height=1100)
     
     # Oszlop: Animáció
     st.markdown("**Animáció:**")
